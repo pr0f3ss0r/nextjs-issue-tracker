4de58743f8b13551807fc15974d899c2e46f1d6d
Pr0f3ss0r <tosindroid@gmail.com>
2023-12-07 15:24:09 +0100

    Setup Radix UI

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,31 +1,37 @@
-'use client'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 import { FaBug } from "react-icons/fa6";
 
 const NavBar = () => {
-const currentPath = usePathname()
+  const currentPath = usePathname();
 
-    const links = [
-        {label:"DashBoard", href: "/"},
-        {label:"Issues", href: "/issues"}
-    ]
+  const links = [
+    { label: "DashBoard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
 
   return (
-    <nav className='flex space-x-5 border-b-2 h-14 items-center px-5 mb-5'>
-        <Link href="/"><FaBug /> </Link>
-       <ul className='flex space-x-5'>
-        {links.map((link)=>(
-             <Link key={link.href}
-             className={`${link.href === currentPath ? 'text-zinc-50' : 'text-zinc-400'  } hover:text-zinc-50 transition-colors`}
-             href={link.href}>
-                {link.label}
-             </Link> 
+    <nav className="flex space-x-5 border-b-2 h-14 items-center px-5 mb-5">
+      <Link href="/">
+        <FaBug />{" "}
+      </Link>
+      <ul className="flex space-x-5">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={`${
+              link.href === currentPath ? "text-zinc-50" : "text-zinc-400"
+            } hover:text-zinc-50 transition-colors`}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
         ))}
-       </ul>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
